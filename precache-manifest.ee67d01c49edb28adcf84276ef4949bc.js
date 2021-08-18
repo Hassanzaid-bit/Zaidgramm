5e5ddc88bebb2779fe3f74559a3c60ffa1d5779a
hassanzaid-bit <hzaid.hz@gmail.com>
2021-08-18 18:14:23 +0300

    Updates

---
+++ precache-manifest.ee67d01c49edb28adcf84276ef4949bc.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e692fedb7075dba215f63e129d9e88af",
+    "url": "/Zaidgramm/index.html"
+  },
+  {
+    "revision": "c5de2c16b71a3468eb24",
+    "url": "/Zaidgramm/static/css/main.d34a1b1b.chunk.css"
+  },
+  {
+    "revision": "f0441023d073f02094b6",
+    "url": "/Zaidgramm/static/js/2.c7a39cde.chunk.js"
+  },
+  {
+    "revision": "f13b8ee62297cdeb55e53ad5d3f0bd7f",
+    "url": "/Zaidgramm/static/js/2.c7a39cde.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c5de2c16b71a3468eb24",
+    "url": "/Zaidgramm/static/js/main.d7656358.chunk.js"
+  },
+  {
+    "revision": "65a50b12e963503b5665",
+    "url": "/Zaidgramm/static/js/runtime-main.e8986566.js"
+  }
+]);
